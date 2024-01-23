e4716ab3309ac0446309cc7a65857790f4525b2c
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-01-23 23:34:24 +0100

    added html, css and js content

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log('Vue OK', Vue);
+
+const {createApp} = Vue
+
+const app = createApp ({
+    data: () => ({
+        tasks: [
+            { id: 1, done: false, text: 'Fare la spesa' },
+            { id: 2, done: true, text: 'Lavare i capelli' },
+            { id: 3, done: false, text: 'Comprare una marca da bollo' },
+            { id: 4, done: false, text: 'Aggiornare il PC' },
+        ]
+    })
+});
+
+app.mount('#root')
