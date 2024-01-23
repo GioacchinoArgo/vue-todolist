0ee02cc8516916360394089740500a2d083b12d4
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-01-24 00:37:20 +0100

    completed first milestone

--- js/script.js
+++ js/script.js
@@ -10,7 +10,17 @@ const app = createApp ({
             { id: 3, done: false, text: 'Comprare una marca da bollo' },
             { id: 4, done: false, text: 'Aggiornare il PC' },
         ]
-    })
+    }),
+    methods: {
+        // Funzione per spuntare gli elementi dalla lista
+        toggleTask(id) {
+            this.tasks.forEach(task => {
+                if (id === task.id) {
+                    task.done = !task.done
+                }
+            })
+        }
+    }
 });
 
 app.mount('#root')
