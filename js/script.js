a234c322ad84006e81194a0576673863b4679c98
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-01-24 01:10:13 +0100

    completed third milestone

--- js/script.js
+++ js/script.js
@@ -9,7 +9,8 @@ const app = createApp ({
             { id: 2, done: true, text: 'Lavare i capelli' },
             { id: 3, done: false, text: 'Comprare una marca da bollo' },
             { id: 4, done: false, text: 'Aggiornare il PC' },
-        ]
+        ],
+        newTask: ''
     }),
     methods: {
         // Funzione per spuntare gli elementi dalla list
@@ -23,6 +24,17 @@ const app = createApp ({
         // Funzione per rimuovere il task
         deleteTask(id) {
             this.tasks = this.tasks.filter(task => id !== task.id);
+        },
+        addTask() {
+            const newTask = { 
+                id: new Date().toISOString(),
+                done: false,
+                text: this.newTaskText
+            }
+            
+            this.tasks.push(newTask);
+
+            this.newTaskText = '';
         }
     }
 });
